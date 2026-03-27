46210b35df9dafd368452b35d2494b99b130e754
ayush <amritayush0722@gmail.com>
2026-03-27 20:13:56 +0530

    Initial commit: basic CLI task manager

---
+++ tasks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+FILE="tasks.txt"
+
+case "$1" in
+  add)
+    echo "$2" >> $FILE
+    echo "Task added!"
+    ;;
+  
+  list)
+    nl -w2 -s'. ' $FILE
+    ;;
+  
+  done)
+    sed -i "${2}s/^/[DONE] /" $FILE
+    echo "Task marked as done!"
+    ;;
+  
+  delete)
+    sed -i "${2}d" $FILE
+    echo "Task deleted!"
+    ;;
+  
+  *)
+    echo "Usage:"
+    echo "./task.sh add \"task\""
+    echo "./task.sh list"
+    echo "./task.sh done <number>"
+    echo "./task.sh delete <number>"
+    ;;
+esac
